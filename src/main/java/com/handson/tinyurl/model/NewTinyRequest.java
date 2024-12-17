6fae05f8c90d78a66a3c6b52b7961e29efde17d6
Nir Katz <99401719+knir44@users.noreply.github.com>
2024-12-17 08:11:39 +0200

    with mongo

--- src/main/java/com/handson/tinyurl/model/NewTinyRequest.java
+++ src/main/java/com/handson/tinyurl/model/NewTinyRequest.java
@@ -2,7 +2,11 @@ package com.handson.tinyurl.model;
 
 public class NewTinyRequest {
     private String longUrl;
+    private  String userName;
 
+    public String getUserName() {
+        return userName;
+    }
     public String getLongUrl() {
         return longUrl;
     }
